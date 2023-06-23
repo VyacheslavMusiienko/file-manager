b6469ecd21c6b27337553727c44be7f31241a172
VyacheslavMusiienko <chuvachok98@gmail.com>
2023-06-23 13:54:17 +0300

    feat: add input operators

--- src/inputVal.js
+++ src/inputVal.js
@@ -1,6 +1,56 @@
 const inputValue = async (value) => {
-  const valueString = value.toString();
-  console.log(valueString);
+  try {
+    const valueFilter = value.toString().trim().split(" ");
+    const [command , ...args] = valueFilter;
+
+    switch (command) {
+      case '.exit':
+        process.exit(0);
+      case 'up':
+        console.log('up');
+        break;
+      case 'cd':
+        console.log('cd');
+        break;
+      case 'ls':
+        console.log('ls');
+        break;
+      case 'cat':
+        console.log('cat');
+        break;
+      case 'add':
+        console.log('add');
+        break;
+      case 'rn':
+        console.log('rn');
+        break;
+      case 'cp':
+        console.log('cp');
+        break;
+      case 'mv':
+        console.log('mv');
+        break;
+      case 'rm':
+        console.log('rm');
+        break;
+      case 'os':
+        console.log('os');
+        break;
+      case 'hash':
+        console.log('hash');
+        break;
+      case 'compress':
+        console.log('compress');
+        break;
+      case 'decompress':
+        console.log('decompress');
+        break;
+      default:
+        throw new Error('Invalid value');
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 export { inputValue };
