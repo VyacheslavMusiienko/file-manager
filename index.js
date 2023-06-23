23c28bca5d9024a1c94ca64dbac90bb0257d8875
VyacheslavMusiienko <chuvachok98@gmail.com>
2023-06-23 12:47:36 +0300

    fix: error catcher in index folder

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ import { homedir } from 'os';
 import { inputValue } from './src/inputVal.js';
 
 const index = async (args) => {
-  const usernameArgs = args.find(arg => arg.startsWith('--username='));
-  process.chdir(homedir());
+  try {
+    const usernameArgs = args.find(arg => arg.startsWith('--username='));
+    process.chdir(homedir());
 
-  if(!usernameArgs){
-    console.log('Invalid input');
+    if(!usernameArgs){
+      throw new Error('Invalid input');
+    }
+
+    const username = usernameArgs.split('=')[1];
+
+    console.log(`Welcome to the File Manager, ${username}!`);
+    console.log(`You are currently in ${process.cwd()}`);
+
+    process.stdin.on('data', inputValue);
+
+    process.on('SIGINT', () => process.exit(0));
+
+    process.on('exit', () => console.log(`Thank you for using File Manager, ${username}, goodbye!`));
+  } catch (error) {
+    console.error(error.message);
   }
-
-  const username = usernameArgs.split('=')[1];
-
-  console.log(`Welcome to the File Manager, ${username}!`);
-  console.log(`You are currently in ${process.cwd()}`);
-
-  process.stdin.on('data', inputValue);
-
-  process.on('SIGINT', () => process.exit(0));
-
-  process.on('exit', () => console.log(`Thank you for using File Manager, ${username}, goodbye!`));
-
 }
 
 index(process.argv.slice(2));
